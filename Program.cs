a430655d75ce91d2a969e9f0f6e903ef0e98335a
Роман Иванов <romaiv99@gmail.com>
2022-03-28 22:14:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Napilnic01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Weapon
+    {
+        public int Damage;
+        public int Bullets;
+
+        public void Fire(Player player)
+        {
+            player.Health -= Damage;
+            Bullets -= 1;
+        }
+    }
+
+    class Player
+    {
+        public int Health;
+    }
+
+    class Bot
+    {
+        public Weapon Weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.Fire(player);
+        }
+    }
+}
